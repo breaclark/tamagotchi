fe42c470ae5b30501edebe06141f23e47a70ae05
Brea Borlas <breatortilla@gmail.com>
2018-04-23 10:32:00 -0700

    Add test for feed function

--- spec/tamagotchi-spec.js
+++ spec/tamagotchi-spec.js
@@ -3,11 +3,17 @@ import { Tamago } from "./../src/tamagotchi.js";
 describe ("Tamago", function () {
   let tamago;
 
-  beforeEach(function() {
+  beforeEach (function () {
     tamago = new Tamago();
   });
 
-  it("should be an instance of the Tamago class", function() {
+  it ("should be an instance of the Tamago class", function () {
     expect(tamago).toEqual(jasmine.any(Tamago));
   });
+
+  it ("should decrease the hunger level by 30", function () {
+    tamago.feed();
+    expect(tamago.hunger).toEqual(0);
+  });
+
 });
