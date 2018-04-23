a461a719b6178d2de20c4ef09cc9d64320d3a071
Brea Borlas <breatortilla@gmail.com>
2018-04-23 11:21:02 -0700

    Instantiate tamago on the front end

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { Tamago } from "./tamagotchi.js";
+
+$(document).ready(function () {
+  let tamago  = new Tamago();
+  $("#hunger h3").text(tamago.hunger);
+  $("#happiness h3").text(tamago.happiness);
+  $("#energy h3").text(tamago.energy);
+  $("#bathroom h3").text(tamago.bathroom);
+});
